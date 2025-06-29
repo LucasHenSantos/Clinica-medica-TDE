d42b319272eb7107b46ab712bfbefba10940ce2c
LucasHenSantos <Lucas.henrique@aluno.imepac.edu.br>
2025-06-29 15:28:18 -0300

    Primeiro

---
+++ src/app/paciente/paciente-adicionar/paciente-adicionar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente-adicionar',
+  templateUrl: './paciente-adicionar.component.html',
+  styleUrls: ['./paciente-adicionar.component.css']
+})
+export class PacienteAdicionarComponent {
+
+}
---
+++ src/app/paciente/paciente-listar/paciente-listar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente-listar',
+  templateUrl: './paciente-listar.component.html',
+  styleUrls: ['./paciente-listar.component.css']
+})
+export class PacienteListarComponent {
+
+}
